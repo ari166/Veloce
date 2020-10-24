889cfeb3b3bb78958effb82e225f84ee2367ab74
ari166 <73370558+ari166@users.noreply.github.com>
2020-10-24 01:36:23 -0700

    Update api.js

--- server/api.js
+++ server/api.js
@@ -7,6 +7,7 @@ var price = 50;
 
 var app = express();
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //Stripe payment route
 app.post("/charge", async (req, res) => {
