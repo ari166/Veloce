cbc8410a146f1a6c081f39d5274fbaf763e5df48
ari166 <73370558+ari166@users.noreply.github.com>
2020-10-23 23:48:47 -0700

    Add files via upload

--- server/api.js
+++ server/api.js
@@ -1,1 +1,35 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const router = express.Router();
+const serverless = require("serverless-http");
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+var price = 50;
 
+var app = express();
+app.use(bodyParser.json());
+
+//Stripe payment route
+app.post("/charge", async (req, res) => {
+  const token = req.body.stripeToken;
+
+  const charge = await stripe.charges.create(
+    {
+      amount: (price * 100).toFixed(0),
+      currency: "usd",
+      description: "Boomer's Tutorial",
+      source: token,
+    },
+    function(err, charge) {
+      if (charge) {
+        res.redirect("/thank-you");
+      }
+      if (err) {
+        res.redirect("/payment");
+      }
+    }
+  );
+});
+
+app.use("/", router);
+
+module.exports.handler = serverless(app);
